cf875c5501c97d4c781a6cae517f3214c25febfb
wilfredkisku <wilfred.kisku@gmail.com>
2021-01-19 19:15:46 +0530

    change in utils, main

--- utils/utilities.py
+++ utils/utilities.py
@@ -10,14 +10,14 @@ from sklearn.feature_extraction import image
 
 import matplotlib.pyplot as plt
 
-count_n = 500
+count_n = 1000
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 
 #recreate an image with pixels removed
-def create(width, height, pp, img):
+def create(pp, img):
     
-    mask = np.random.choice([0, 1], size=(width, height), p=[1-pp, pp])
+    mask = np.random.choice([0, 1], size=(IMG_WIDTH, IMG_HEIGHT), p=[1-pp, pp])
     idx_w, idx_h = np.where(mask ==  1)
     
     for i in range(len(idx_w)):
@@ -32,13 +32,19 @@ def curate():
     for idx, j in enumerate(lst):
         onlyfiles   = [f for f in listdir(lst[idx]) if isfile(join(lst[idx], f))]
         onlyfiles.remove('Thumbs.db')
-        cnt += len(onlyfiles)
+
         for _, i in enumerate(onlyfiles):
             p = join(str(lst[idx]),i)
             img = cv2.imread(p, 0)
-            img = create(img.shape[0], img.shape[1], 0.1, img)
-            imgplot = plt.imshow(img, cmap='gray', vmin = 0, vmax = 255)
-            plt.show()
+            x_patches = image.extract_patches_2d(img, (128,128), max_patches = 2)
+            img = create(0.1, img)
+            y_patches = image.extract_patches_2d(img, (128,128), max_patches = 2)
+            #imgplot = plt.imshow(img, cmap='gray', vmin = 0, vmax = 255)
+            #plt.show()
+            for k in range(len(x_patches)):
+                X_train[cnt] = x_patches[k]
+                Y_train[cnt] = y_patches[k]
+                cnt += 1
 
 #changes need to be made
 def printResult():
@@ -48,32 +54,32 @@ def printResult():
     print(y_vari.shape,y_predi.shape)
 
     for i in range(10,20):
-    img = X_var[i]
-    segpred = y_predi[i]
-    seg = y_vari[i]
+        img = X_var[i]
+        segpred = y_predi[i]
+        seg = y_vari[i]
 
-    fig = plt.figure(figsize=(15,5))
-    ax = fig.add_subplot(1,3,1)
-    ax.imshow(img)
-    ax.set_title("original")
+        fig = plt.figure(figsize=(15,5))
+        ax = fig.add_subplot(1,3,1)
+        ax.imshow(img)
+        ax.set_title("original")
 
-    ax = fig.add_subplot(1,3,2)
-    ax.imshow(design_colormap(segpred,n_classes))
-    ax.set_title("FCN")
+        ax = fig.add_subplot(1,3,2)
+        ax.imshow(design_colormap(segpred,n_classes))
+        ax.set_title("FCN")
 
-    ax = fig.add_subplot(1,3,3)
-    ax.imshow(design_colormap(seg,n_classes))
-    ax.set_title("Ground True")
-    plt.show()
+        ax = fig.add_subplot(1,3,3)
+        ax.imshow(design_colormap(seg,n_classes))
+        ax.set_title("Ground True")
+        plt.show()
 
     return None 
 
 if __name__ == "__main__":
 
-    ########################## CREATE DATASET ###########################
-    X_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
-    Y_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
-    
-    #####################################################################
+    #create dataset for images
+    ########################## CREATE DATASET #############################
+    X_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH), dtype=np.uint8)
+    Y_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH), dtype=np.uint8) 
+    #######################################################################
     curate()
 
