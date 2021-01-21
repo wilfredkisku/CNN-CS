b893b46e73d9eb27f35ab502b8fb95eabecb1814
wilfredkisku <wilfred.kisku@gmail.com>
2021-01-21 23:01:09 +0530

    changes in main_new

--- main_new.py
+++ main_new.py
@@ -12,6 +12,48 @@ count_n = 500 * 8
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 
+def ssim_loss(true, pred):
+  return tf.reduce_mean(tf.image.ssim(true, pred, 1.0))
+
+def networkCSNet(x_train, y_train, x_val, y_val):
+
+    input_layer = tf.keras.layers.Input((128, 128, 1))
+    
+    conv1 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(input_layer)
+    conv1 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv1)
+    conv1 = tf.keras.layers.ReLU()(conv1)
+
+    conv2 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv1)
+    conv2 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv2)
+    conv2 = tf.keras.layers.ReLU()(conv2)
+
+    conv3 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv2)
+    conv3 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv3)
+    conv3 = tf.keras.layers.ReLU()(conv3)
+
+    conv4 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv3)
+    conv4 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv4)
+    conv4 = tf.keras.layers.ReLU()(conv4)
+
+    conv5 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv4)
+    conv5 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv5)
+    conv5 = tf.keras.layers.ReLU()(conv5)
+
+    conv6 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv5)
+    conv6 = tf.keras.layers.Conv2D(1, (3,3), padding='same')(conv6)
+    conv6 = tf.keras.layers.ReLU()(conv6)
+
+    output_layer = tf.keras.layers.Conv2D(1, (1,1), padding='same', activation='sigmoid')(conv6)
+
+    ae = tf.keras.models.Model(inputs = [input_layer], outputs = [output_layer])
+    ae.compile(optimizer='adam', loss=ssim_loss)
+
+    ae.summary()
+
+    history = ae.fit(x_train, y_train, epochs=35, batch_size=64, shuffle=True, validation_data=(x_val, y_val), verbose = 1)
+
+    return ae
+
 def networkNew(x_train, y_train, x_val, y_val):
 
     input_layer = tf.keras.layers.Input((128, 128, 1))
@@ -63,7 +105,17 @@ def networkNew(x_train, y_train, x_val, y_val):
 
     ae.summary()
 
-    history = ae.fit(x_train, y_train, epochs=35, batch_size=64, shuffle=True, validation_data=(x_val, y_val), verbose = 1)
+    checkpointer = ModelCheckpoint('models/cs-unet-500.h5', verbose=1, save_best_only=True)
+    history = ae.fit(x_train, y_train, epochs=500, batch_size=64, shuffle=True, validation_data=(x_val, y_val), verbose = 1)
+
+    hist_df = pd.DataFrame(history.history)
+    hist_csv_file = 'models/cs-unet-history.csv'
+
+    with open(hist_csv_file, mode = 'w') as f:
+        hist_df.to_csv(f)
+
+    print('End of training ...')
+    
     return ae
 
 def network(x_train, y_train, x_val, y_val):
@@ -141,9 +193,8 @@ if __name__ == "__main__":
     plt.show()
     '''
     #model_cnn = network(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
-    print(X_VAL[:10,:,:,:].shape)
-    
-    model_cnn = networkNew(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
+    #model_cnn = networkNew(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
+    model_cnn = networkCSNet(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
     predict = model_cnn.predict(X_VAL[:10,:,:,:])
 
     fig = plt.figure(figsize=(9, 4))
