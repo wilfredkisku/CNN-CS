517d4ea84e011686d77ca44cea8ba2818495d559
wilfredkisku <wilfred.kisku@gmail.com>
2021-02-01 12:51:49 +0530

    changes in main

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
-from utils.utilities import curate
+from utils.utilities_new import curate
 
-count_n = 500 * 8
+count_n = 500 * 10
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 
@@ -16,75 +16,36 @@ def network(x_train, y_train, x_val, y_val):
 
     input_img = tf.keras.layers.Input(shape=(128, 128, 1))
     
-    #input_norm = tf.keras.layers.Lambda(lambda x:x / 255)(input_img)
-
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(input_img)
-    x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    #x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    #x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    encoded = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(encoded)
-    x = tf.keras.layers.UpSampling2D((2, 2))(x)
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    x = tf.keras.layers.UpSampling2D((2, 2))(x)
-    #x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    #x = tf.keras.layers.UpSampling2D((2, 2))(x)
-    x = tf.keras.layers.Conv2D(3, (3, 3), activation='relu', padding='same')(x)
-    x = tf.keras.layers.UpSampling2D((2, 2))(x)
-
-    decoded = tf.keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    y = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid', padding='same')(x)
     
     ae = tf.keras.models.Model(inputs = [input_img], outputs = [decoded])
-    ae.compile(optimizer='adam', loss='MSE', metrics=['accuracy'])
+    ae.compile(optimizer='adam', loss=ssim_loss)
 
     ae.summary()
 
-    history = ae.fit(x_train, y_train, epochs=35, batch_size=64, shuffle=True, validation_data=(x_val, y_val), verbose = 2)
+    history = ae.fit(x_train, y_train, epochs=1000, batch_size=128, shuffle=True, validation_data=(x_val, y_val), verbose = 1)
 
 
 if __name__ == "__main__":
-    #create dataset for images
-    ########################## CREATE DATASET #############################
+    
+    #####################################################################
+    ########################## CREATE DATASET ###########################
+    #####################################################################
+
     X_train = np.zeros((count_n, 128, 128, 1), dtype=np.uint8)
     Y_train = np.zeros((count_n, 128, 128, 1), dtype=np.uint8)
-    #######################################################################
-    #Get the curated data from the dataset
         
     X, Y = curate(X_train, Y_train)
     
     X = X / 255
     Y = Y / 255
 
-    #print((X[0]))
-
-    '''
-    plt.imshow(Y[0], cmap='gray')
-    plt.show()
-
-    plt.imshow(X[0], cmap='gray')
-    plt.show()
-    '''
-    
     X_TRAIN, X_VAL, Y_TRAIN, Y_VAL = train_test_split(X, Y, test_size = 0.2, random_state = 42)
-   
-    '''
-    print(X_TRAIN.shape)
-    print(X_VAL.shape)
-    print(Y_TRAIN.shape)
-    print(Y_VAL.shape)
-
-    plt.imshow(X_TRAIN[0])
-    plt.show()
-    plt.imshow(Y_TRAIN[0])
-    plt.show()
-
-    plt.imshow(X_VAL[0])
-    plt.show()
-    plt.imshow(Y_VAL[0])
-    plt.show()
-    '''
     network(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
     
