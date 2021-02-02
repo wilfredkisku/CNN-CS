f52ec42cde6443e940ba40edfdfee883195f5859
wilfredkisku <wilfred.kisku@gmail.com>
2021-02-02 18:39:52 +0530

    changes main main_sqnet

--- main_new.py
+++ main_new.py
@@ -8,10 +8,14 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from utils.utilities_new import curate
 
-def ssim_loss(true, pred):
-  return 1 - tf.reduce_mean(tf.image.ssim(true, pred, 1.0))
+def psnr_mean(y_true, y_pred):
+    return tf.reduce_mean(tf.image.psnr(y_true, y_pred, max_val=1.0))
 
-def networkCSNet(x_train, y_train, x_val, y_val):
+
+def ssim_loss(true, pred):
+    return 1 - tf.reduce_mean(tf.image.ssim(true, pred, 1.0))
+
+def SimpleCSNet(x_train, y_train, x_val, y_val):
 
     input_layer = tf.keras.layers.Input((128, 128, 1))
     
@@ -39,39 +43,7 @@ def networkCSNet(x_train, y_train, x_val, y_val):
     ae.compile(optimizer='adam', loss=ssim_loss)
 
     ae.summary()
-    checkpointer = ModelCheckpoint('models/cs-net-1000.h5', verbose=1, save_best_only=True)
-    history = ae.fit(x_train, y_train, epochs=1000, batch_size=16, shuffle=True, validation_data=(x_val, y_val), verbose = 1,  callbacks=[checkpointer])
 
-    hist_df = pd.DataFrame(history.history)
-    hist_df.to_csv('models/cs-net-history-1000.csv')
-    print('End of training ...')
-
-    return ae
-
-def network():
-
-    input_img = tf.keras.layers.Input(shape=(128, 128, 1))
-    
-    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(input_img)
-    
-    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    
-    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    
-    x = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-    
-    x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    
-    x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-
-    output_img = tf.keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-    
-    ae = tf.keras.models.Model(inputs = [input_img], outputs = [output_img])
-    ae.compile(optimizer='adam', loss=ssim_loss)
-
-    ae.summary()
-
-    history = ae.fit(x_train, y_train, epochs=1000, batch_size=32, shuffle=True, validation_data=(x_val, y_val), verbose = 1)
     return ae
 
 if __name__ == "__main__":
@@ -80,10 +52,7 @@ if __name__ == "__main__":
     ################ TRAINING AND VALIDATING ################
     #########################################################
     
-    #model = network()
-    #model.summary()
-
-    count_n = 500 * 8
+    count_n = 500 * 10
     IMG_WIDTH = 128
     IMG_HEIGHT = 128
 
@@ -97,8 +66,15 @@ if __name__ == "__main__":
 
     X_TRAIN, X_VAL, Y_TRAIN, Y_VAL = train_test_split(X, Y, test_size = 0.2, random_state = 42)
    
-    model_cnn = networkCSNet(X_TRAIN, Y_TRAIN, X_VAL, Y_VAL)
+    model_cnn = SimpleCSNet()
     
+    checkpointer = tf.keras.callbacks.ModelCheckpoint('/workspace/data/cs-hi-1000.h5', verbose=1, save_best_only=True)
+    history = model_cnn.fit(X_TRAIN, Y_TRAIN, epochs=1000, batch_size=32, shuffle=True, validation_data=(X_TEST, Y_TEST), verbose = 1,  callbacks=[checkpointer])
+
+    hist_df = pd.DataFrame(history.history)
+    hist_df.to_csv('/workspace/data/cs-hi-history-1000.csv')
+    print('End of training ...')
+
     predict = model_cnn.predict(X_VAL[:10,:,:,:])
     x_val = X_VAL[:10,:,:,:]
     y_val = Y_VAL[:10,:,:,:]
--- main_sqnet.py
+++ main_sqnet.py
@@ -11,6 +11,12 @@ def curateData():
 
     return x_train, y_train, x_test, y_test
 
+def psnr_mean(y_true, y_pred):
+    return tf.reduce_mean(tf.image.psnr(y_true, y_pred, max_val=1.0))
+
+def ssim_loss(y_true, y_pred):
+    return (1 - tf.reduce_mean(tf.image.ssim(y_true, y_pred, 1.0))) + tf.reduce_mean(tf.image.psnr(y_true, y_pred, 1.0))
+
 def fire(x, squeeze, expand):
     y  = tf.keras.layers.Conv2D(filters=squeeze, kernel_size=1, activation='relu', padding='same')(x)
     y1 = tf.keras.layers.Conv2D(filters=expand//2, kernel_size=1, activation='relu', padding='same')(y)
@@ -20,24 +26,47 @@ def fire(x, squeeze, expand):
 def fire_module(squeeze, expand):
     return lambda x: fire(x, squeeze, expand)
 
-input_layer = tf.keras.layers.Input(shape=[128, 128, 1])
-y = tf.keras.layers.Conv2D(kernel_size=3, filters=32, padding='same', use_bias=True, activation='relu')(x)
-y = fire_module(32, 32)(y)
-y = fire_module(32, 32)(y)
-y = fire_module(32, 32)(y)
-y = fire_module(32, 32)(y)
-y = fire_module(32, 32)(y)
-output_layer = tf.keras.layers.Conv2D(1, (1,1), padding = 'same', activation='sigmoid')(y)
+def SimpleCSNet_sq():
+    input_layer = tf.keras.layers.Input(shape=[128, 128, 1])
+    y = tf.keras.layers.Conv2D(kernel_size=3, filters=32, padding='same', use_bias=True, activation='relu')(x)
+    y = fire_module(32, 32)(y)
+    y = fire_module(32, 32)(y)
+    y = fire_module(32, 32)(y)
+    y = fire_module(32, 32)(y)
+    y = fire_module(32, 32)(y)
+    output_layer = tf.keras.layers.Conv2D(1, (1,1), padding = 'same', activation='sigmoid')(y)
 
-ae = tf.keras.models.Model(inputs = [input_layer], outputs = [output_layer])
-ae.compile(optimizer='adam', loss=ssim_loss, metrics = [psnr_mean])
-ae.summary()
+    ae = tf.keras.models.Model(inputs = [input_layer], outputs = [output_layer])
+    ae.compile(optimizer='adam', loss=ssim_loss, metrics = [psnr_mean])
+    ae.summary()
 
-X_train, Y_train, X_test, Y_test = curateData()
+    return ae
 
-checkpointer = tf.keras.callbacks.ModelCheckpoint('/workspace/data/cs-1000.h5', verbose = 1, save_best_only = True)
-history = ae.fit(X_train, Y_train, epochs = 1000, batch_size = 32, shuffle=True, validation_data = (X_test, Y_test), verbose =1 , callbacks=[checkpointer])
+if __name__ == "__main__":
+    
+    #########################################################
+    ################ TRAINING AND VALIDATING ################
+    #########################################################
 
-hist_df = pd.DataFrame(history.history)
-hist_df.to_csv('cs-history-1000.csv')
+    count_n = 500 * 10
+    IMG_WIDTH = 128
+    IMG_HEIGHT = 128
 
+    X_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
+    Y_train = np.zeros((count_n, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.uint8)
+
+    X, Y = curateData(X_train, Y_train)
+
+    X = X / 255.
+    Y = Y / 255.
+
+    X_TRAIN, X_VAL, Y_TRAIN, Y_VAL = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+    model_cnn = SimpleCSNet_sq()
+
+    checkpointer = tf.keras.callbacks.ModelCheckpoint('/workspace/data/cs-sq-1000.h5', verbose = 1, save_best_only = True)
+    history = ae.fit(X_TRAIN, Y_TRAIN, epochs = 1000, batch_size = 32, shuffle=True, validation_data = (X_VAL, Y_VAL), verbose =1 , callbacks=[checkpointer])
+
+    hist_df = pd.DataFrame(history.history)
+    hist_df.to_csv('/workspace/data/cs-sq-history-1000.csv')
+    print('End of training ...')
