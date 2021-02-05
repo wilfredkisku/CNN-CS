c1b1c2df370ca9196cdd669a2061fed3fb3e587c
wilfredkisku <wilfred.kisku@gmail.com>
2021-02-06 01:16:19 +0530

    changes in all

--- utils/utilities_new.py
+++ utils/utilities_new.py
@@ -70,6 +70,64 @@ def curate_(path, lt,  x, y):
                 y[cnt] = y_patches[k]
                 cnt += 1
 
+def imgSave():
+    
+    x_ = np.zeros((10, 128, 128, 1), dtype=np.uint8)
+    y_ = np.zeros((10, 128, 128, 1), dtype=np.uint8)
+
+    data = Path('/workspace/storage/cnn-cs/data/test')
+    lst = os.listdir(data)
+    lst.sort()
+
+    count = 0
+
+    for _, i in enumerate(lst):
+        p = join(data,i)
+        img = cv2.imread(p, 0)
+
+        y_patches = image.extract_patches_2d(img, (128, 128), max_patches = 1)
+        y_patches = np.reshape(y_patches,(1, 128, 128,-1))
+        y_patch = copy.deepcopy(y_patches)
+        x_patches = create(0.10, y_patch, 1)
+
+        for k in range(len(x_patches)):
+            x_[count] = x_patches[k]
+            y_[count] = y_patches[k]
+            count += 1
+
+    x_ = x_ / 255.
+    y_ = y_ / 255.
+
+    fig = plt.figure(figsize=(25, 25))
+    columns = 10
+    rows = 1
+    for i in range(1, columns*rows + 1):
+        img_x = x_[i-1]
+        ax = fig.add_subplot(rows, columns, i)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.imshow(np.reshape(img_x,(128,128)),cmap='gray')
+
+    plt.savefig('/workspace/data/image_sparse.png')
+    
+    model = tf.keras.models.load_model('/workspace/data/cs-simple-model-1000.h5', compile =False)
+    
+    predict = model.predict(x_[:10,:,:,:])
+    
+    fig = plt.figure(figsize=(25, 25))
+    columns = 10
+    rows = 1
+    for i in range(1, columns*rows + 1):
+        img_x = predict[i-1]
+        ax = fig.add_subplot(rows, columns, i)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.imshow(np.reshape(img_x,(128,128)), cmap='gray')
+
+    plt.savefig('/workspace/data/image_recons.png')
+    psnr_ = tf.image.psnr(y_[:10,:,:,:], predict, max_val=1.0)
+    print(psnr_)
+
 def printResult(X, Y):
 
     fig = plt.figure(figsize=(9, 4))
